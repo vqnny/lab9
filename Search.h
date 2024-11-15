8d953caf3e20a0e50fa0160848619511b8cfd580
cyn <115718204+vqnny@users.noreply.github.com>
2024-11-14 21:02:24 -0600

    Update Search.h

--- Search.h
+++ Search.h
@@ -6,8 +6,22 @@ using namespace std;
 // write a function to print the contents of your array
 //   bonus! try to implement your function so that it can accept
 //     many types of data (int, char, string, etc.)
-template <typename flexibleType> // ???
-void printArray(...) {}
+template <typename T> // ???
+void printArray(T *array, int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int intArray[] = {1, 2, 3, 4, 5};
+    char charArray[] = {'a', 'b', 'c', 'd', 'e'};
+    string stringArray[] = {"hello", "world", "C++", "templates"};
+    printArray(intArray, 5);
+    printArray(charArray, 5);
+    printArray(stringArray, 4);
+    return 0;
+}
 
 // Implement a sequential search algorithm
 // your function should search for a target value (target)
@@ -15,25 +29,83 @@ void printArray(...) {}
 // return true if target exists in the array within this range,
 //    return false otherwise
 bool seqSearch(string target, string arr[], int start, int end) {
+    if (start < 0 || end < 0 || start > end) {
+        return false;
+    }
+    for (int i = start; i <= end; ++i) {
+        if (arr[i] == target) {
+            return true;
+        }
+    }
     return false;
+}
+int main() {
+    string arr[] = {"apple", "banana", "cherry", "date", "elderberry"};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << (seqSearch("cherry", arr, 1, 3) ? "Found" : "Not Found") << endl;
+    cout << (seqSearch("apple", arr, 2, 4) ? "Found" : "Not Found") << endl;
+
+    return 0;
 }
 
 // Implement an iterative binary search 
 // Return true if target exists in the array with size n,
 //    return false otherwise 
 bool binSearch(float target, float arr[], int n) {
+    int left = 0;
+    int right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return true;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
     return false;	
+}
+int main() {
+    float arr[] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6}; // Sorted array
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << (binSearch(3.3, arr, size) ? "Found" : "Not Found") << endl;
+    cout << (binSearch(7.7, arr, size) ? "Found" : "Not Found") << endl;
+
+    return 0;
 }
 
 // Implement a recursive binary search 
 // Return true if target exists in the array with size n
 //   return false otherwise
-bool binSearchR(char target, char charray[], int n) {
+bool binSearchHelper(char target, char charray[], int left, int right) {
     // base case
+    if (left > right) {
+        return false;
+    }
+    int mid = left + (right - left) / 2;
+    if (charray[mid] == target) {
+        return true;
+    }
+    else if (charray[mid] < target) {
+        return binSearchHelper(target, charray, left, mid - 1);
+    }
+}
+bool binSearchR(char target, char charray[], int n)  { 
+    return binSearchHelper(target, charray, 0, n - 1);
+}
+int main() {
+    char charray[] = {'a', 'c', 'e', 'g', 'i', 'k', 'm'};
+    int size = sizeof(charray) / sizeof(charray[0]);
 
-    //general case
+    cout << (binSearchR('e', charray, size) ? "Found" : "Not Found") << endl;
+    cout << (binSearchR('z', charray, size) ? "Found" : "Not Found") << endl;
 
-    return false;
+    return 0;
 }
 
 // Implement a brand new sorting algorithm
@@ -57,10 +129,35 @@ Step 3: Finally, use your two functions above to complete the following in newSo
         *** You can make this recursive, if you wish!
 */
 
-void swap(double darray[], ...) {}
-
-int minFind(double darray[], ...) {
-    return -1;
+void swap(double darray[], int index1, int index2) {
+    double temp = darray[index1];
+    darray[index1] = darray[index2];
+    darray[index2] = temp;
 }
 
-void newSort(double darray[], int n) {}
+int minFind(double darray[], int start, int n) {
+    int minIndex = start;
+    for (int i = start + 1; i < n; ++i) {
+        if (darray[i] < darray[minIndex]) {
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+void newSort(double darray[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = minFind(darray, i, n);
+        swap(darray, i, minIndex);
+    }
+}
+int main() {
+    double darray[] = {3.5, 2.1, 5.7, 1.8, 4.3};
+    int n = sizeof(darray) / sizeof(darray[0]);
+    newSort(darray, n);
+    for (int i = 0; i < n; ++i) {
+        cout << darray[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
